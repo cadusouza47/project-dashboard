35303fe9f39281a4f24ba259ce3018232da56322
Carlos Eduardo Batista de Souza <carlos.souza@bandtec.com.br>
2022-09-13 23:51:00 -0300

    Ajuste chart bar

--- src/pages/Charts/Bar.jsx
+++ src/pages/Charts/Bar.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { ChartComponent, SeriesCollectionDirective,
 SeriesDirective, Inject, Legend, Category, Tooltip, ColumnSeries, DataLabel } from '@syncfusion/ej2-react-charts';
 
-import { ChartsHeader } from '../../components';
+import { Header } from '../../components';
 
 import { barCustomSeries, barPrimaryXAxis,
 barPrimaryYAxis } from '../../data/dummy';
@@ -15,7 +15,7 @@ const Area = () => {
   return (
     <div className='m-4 md:m-10 mt-24 p-10 bg-white
     dark:bg-secondary-dark-bg rounded-3xl'>
-      <ChartsHeader category="Bar" title="Olympic Medal Counts - RIO"/>
+      <Header category="Bar" title="Olympic Medal Counts - RIO"/>
     
    <div className='m-4 md:m-10 mt-24 p-10 bg-white dark:bg-secondary-dark-bg rounded-3xl'>
     <ChartComponent
